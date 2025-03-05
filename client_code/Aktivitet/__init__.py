58df0eb93f98d8fc232251ff50afcda04aab07e2
qyvindf@gmail.com <qyvindf@gmail.com>
2025-03-05 20:53:57 +0000

    Created Aktivitet.RowTemplate1, Aktivitet.Form2 and Aktivitet; Edited settings; Deleted Form1

---
+++ client_code/Aktivitet/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import AktivitetTemplate
+from anvil import *
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+
+
+class Aktivitet(AktivitetTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.aktivitet.search()
+
+    # Any code you write here will run before the form opens.
