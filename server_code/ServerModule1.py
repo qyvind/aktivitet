2b0eb511e483f7d4bd62d049bbcfbc1353973adb
qyvindf@gmail.com <qyvindf@gmail.com>
2025-03-06 13:08:12 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,6 +9,10 @@ import anvil.server
 
 @anvil.server.callable
 def add_aktivitet(aktivitet):
-  print('server called')
   if aktivitet.get('dato') and aktivitet.get('aktivitet') and aktivitet.get('poeng') :
-     app_tables.aktivitet.add_row(**aktivitet)
+    app_tables.aktivitet.add_row(**aktivitet)
+
+@anvil.server.callable
+def update_aktivitet(aktivitet)
+  if movie_data['director'] and movie_data['movie_name'] and movie_data['summary'] and movie_data['year']:
+    movie.update(**movie_data)
