57d224bee312fb59f449ed591fe1578f29b56860
qyvindf@gmail.com <qyvindf@gmail.com>
2025-04-17 17:30:52 +0000

    Edited Loggbok

--- client_code/Loggbok/__init__.py
+++ client_code/Loggbok/__init__.py
@@ -272,6 +272,7 @@ class Loggbok(LoggbokTemplate):
   
     def vis_tildelte_badges(self, bruker):
       # Skjul alle badges først
+      self.badge_flow_panel.visible = False
       for i in range(1, 10):  # Tilpass til maks antall badges du har
           badge_komponent = getattr(self, f"badge_{i}", None)
           if badge_komponent:
@@ -284,6 +285,7 @@ class Loggbok(LoggbokTemplate):
           badge_id = badge['id']
           badge_komponent = getattr(self, f"badge_{badge_id}", None)
           if badge_komponent:
+              self.badge_flow_panel.visible = True
               badge_komponent.visible = True
 
       
@@ -573,28 +575,28 @@ class Loggbok(LoggbokTemplate):
       """This method is called when the button is clicked"""
       open_form(('Supertrekning'))
 
-    def badge_1_mouse_enter(self, x, y, **event_args):
-      """This method is called when the mouse cursor enters this component"""
-      self.badge_1.source = '_/theme/icons8-three-leaf-clover-48.png'
+    # def badge_1_mouse_enter(self, x, y, **event_args):
+    #   """This method is called when the mouse cursor enters this component"""
+    #   self.badge_1.source = '_/theme/icons8-three-leaf-clover-48.png'
 
-    def badge_1_mouse_leave(self, x, y, **event_args):
-      """This method is called when the mouse cursor leaves this component"""
-      self.badge_1.source = '_/theme/icons8-three-leaf-clover-48 BW.png'
+    # def badge_1_mouse_leave(self, x, y, **event_args):
+    #   """This method is called when the mouse cursor leaves this component"""
+    #   self.badge_1.source = '_/theme/icons8-three-leaf-clover-48 BW.png'
 
-    def badge_2_mouse_enter(self, x, y, **event_args):
-      """This method is called when the mouse cursor enters this component"""
-      self.badge_2.source = '_/theme/icons8-777-48.png'
+    # def badge_2_mouse_enter(self, x, y, **event_args):
+    #   """This method is called when the mouse cursor enters this component"""
+    #   self.badge_2.source = '_/theme/icons8-777-48.png'
 
-    def badge_2_mouse_leave(self, x, y, **event_args):
-      """This method is called when the mouse cursor leaves this component"""
-      self.badge_2.source = '_/theme/icons8-777-48 BW.png'
+    # def badge_2_mouse_leave(self, x, y, **event_args):
+    #   """This method is called when the mouse cursor leaves this component"""
+    #   self.badge_2.source = '_/theme/icons8-777-48 BW.png'
 
-    def badge_3_mouse_enter(self, x, y, **event_args):
-      self.badge_3.source='_/theme/icons8-triathlon-64.png'
+    # def badge_3_mouse_enter(self, x, y, **event_args):
+    #   self.badge_3.source='_/theme/icons8-triathlon-64.png'
 
-    def badge_3_mouse_leave(self, x, y, **event_args):
-      """This method is called when the mouse cursor leaves this component"""
-      self.badge_3.source = '_/theme/icons8-triathlon-64 BW.png'
+    # def badge_3_mouse_leave(self, x, y, **event_args):
+    #   """This method is called when the mouse cursor leaves this component"""
+    #   self.badge_3.source = '_/theme/icons8-triathlon-64 BW.png'
 
     def vis_nye_badges(self,bruker):
         nye_badger = app_tables.user_badges.search(user=bruker, informert=False)
