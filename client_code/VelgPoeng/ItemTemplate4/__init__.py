56dbc5690e342ba42522ff04c226f6814751cd36
qyvindf@gmail.com <qyvindf@gmail.com>
2025-04-18 08:56:57 +0000

    Created VelgPoeng.ItemTemplate4; Edited settings and VelgPoeng

---
+++ client_code/VelgPoeng/ItemTemplate4/__init__.py
@@ -0,0 +1,84 @@
+from ._anvil_designer import ItemTemplate4Template
+from anvil import *
+import anvil.server
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class ItemTemplate4(ItemTemplate4Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+            # Hent ut data for hvert ikon (hvis det finnes)
+    icon_data_1 = self.item.get('icon_1')
+    icon_data_2 = self.item.get('icon_2')
+    icon_data_3 = self.item.get('icon_3')
+    icon_data_4 = self.item.get('icon_4')
+
+    # --- Sett egenskaper for Bilde 1 ---
+    if icon_data_1:
+        self.image_1.source = icon_data_1.get('media')
+        # Hent det pene navnet fra 'path' og sett det som tooltip
+        self.label_1.text = icon_data_1.get('path', '') # Bruker .get() for sikkerhets skyld
+        self.image_1.tag = icon_data_1 # Lagre hele dictionaryen i tag
+    else:
+        # Håndter tilfellet der det ikke er noe ikon på denne plassen
+        self.image_1.source = None
+        self.label_1.text = "" # Ingen tooltip for tom plass
+        self.image_1.tag = None
+
+    # --- Sett egenskaper for Bilde 2 ---
+    if icon_data_2:
+        self.image_2.source = icon_data_2.get('media')
+        self.label_2.text = icon_data_2.get('path', '')
+        self.image_2.tag = icon_data_2
+    else:
+        self.image_2.source = None
+        self.label_2.text = ""
+        self.image_2.tag = None
+
+    # --- Sett egenskaper for Bilde 3 ---
+    if icon_data_3:
+        self.image_3.source = icon_data_3.get('media')
+        self.label_3.text = icon_data_3.get('path', '')
+        self.image_3.tag = icon_data_3
+    else:
+        self.image_3.source = None
+        self.label_3.text = ""
+        self.image_3.tag = None
+
+    # --- Sett egenskaper for Bilde 4 ---
+    if icon_data_4:
+        self.image_4.source = icon_data_4.get('media')
+        self.label_4.text = icon_data_4.get('path', '')
+        self.image_4.tag = icon_data_4
+    else:
+        self.image_4.source = None
+        self.label_4.text = ""
+        self.image_4.tag = None
+
+  def image_click(self, x, y, button, keys, **event_args):
+        clicked_image_component = event_args['sender']
+
+        # --- MODIFISERT DEL START ---
+        # Hent den lagrede data-dictionaryen fra tag'en
+        selected_icon_data = clicked_image_component.tag
+
+        if selected_icon_data: # Sjekk at vi klikket på et bilde med data
+            # selected_icon_data er nå {'media': MediaObject, 'path': 'stien/til/fil.png'}
+          
+
+        #     if self.parent:
+        #         # Send hele data-dictionaryen oppover
+        #         self.parent.raise_event('x-icon-click', icon_data=selected_icon_data)
+        #     else:
+        #         print("ItemTemplate1 (Ikon) FEIL: Kunne ikke finne self.parent!")
+        # # --- MODIFISERT DEL SLUTT ---
+        # else:
+        #     print("ItemTemplate1 (Ikon): Klikket på en tom bildeplass.")
--- client_code/VelgPoeng/__init__.py
+++ client_code/VelgPoeng/__init__.py
@@ -14,8 +14,36 @@ class VelgPoeng(VelgPoengTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.poeng_drop.items = [("Kortere enn en halv time", 0), ("En halv time", 1), ("En time", 2), ("Halvannen time eller mer", 3)]
+    self.poeng_drop.selected_value = 0
+    all_icon_data = [{'media': r['file'], 'path': r['path']}
+                      for r in app_tables.files.search()
+                      if r['file'] and r['path']] # Sørg for at begge feltene finnes
 
-    # Any code you write here will run before the form opens.
+    # Group icon data into lists of 4 for the repeating panel
+    grouped_icon_data = []
+    for i in range(0, len(all_icon_data), 4):
+        # Create a dictionary for each row in the repeating panel
+        # Hver 'icon_x' inneholder nå en dictionary med 'media' og 'path'
+        row_data = {
+            'icon_1': all_icon_data[i]   if i < len(all_icon_data) else None,
+            'icon_2': all_icon_data[i+1] if i+1 < len(all_icon_data) else None,
+            'icon_3': all_icon_data[i+2] if i+2 < len(all_icon_data) else None,
+            'icon_4': all_icon_data[i+3] if i+3 < len(all_icon_data) else None,
+        }
+        grouped_icon_data.append(row_data)
 
+    self.ikon_repeating_panel.items = grouped_icon_data
+
+    
   def registrer_trening_click(self, **event_args):
-    self.skritt_panel.visible = False
+    self.skritt_panel_1.visible = False
+    self.skritt_panel_2.visible = False 
+    self.trening_panel_1.visible = True
+    self.trening_panel_2.visible = True 
+
+  def button_1_click(self, **event_args):
+    self.skritt_panel_1.visible = True
+    self.skritt_panel_2.visible = True
+    self.trening_panel_1.visible = False
+    self.trening_panel_2.visible = False
