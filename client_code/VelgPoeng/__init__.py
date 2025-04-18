7657c9ba4207326253e8b9c000e330b373ecea7f
qyvindf@gmail.com <qyvindf@gmail.com>
2025-04-18 07:23:26 +0000

    Created VelgPoeng; Edited Loggbok

---
+++ client_code/VelgPoeng/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import VelgPoengTemplate
+from anvil import *
+import anvil.server
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class VelgPoeng(VelgPoengTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
