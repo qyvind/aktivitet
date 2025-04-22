53b4d3dd909f8bdd2c65ada5f125218821353942
qyvindf@gmail.com <qyvindf@gmail.com>
2025-04-22 10:10:32 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -14,6 +14,10 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    bruker = anvil.users.get_user()
+    if bruker:
+        open_form('Loggbok')
+    
 
     # Any code you write here will run before the form opens.
 
