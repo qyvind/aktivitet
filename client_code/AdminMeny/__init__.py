6eed5c769ea0933686d9e56c8d728bc4234ad76e
qyvindf@gmail.com <qyvindf@gmail.com>
2025-04-25 15:43:01 +0000

    Created endringslogg_old and AdminMeny; Edited endringslogg

--- client_code/AdminMeny/__init__.py
+++ client_code/AdminMeny/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import endringsloggTemplate
+from ._anvil_designer import AdminMenyTemplate
 from anvil import *
 import anvil.server
 import anvil.facebook.auth
@@ -10,7 +10,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class endringslogg(endringsloggTemplate):
+class AdminMeny(AdminMenyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
--- client_code/endringslogg_old/__init__.py
+++ client_code/endringslogg_old/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import endringsloggTemplate
+from ._anvil_designer import endringslogg_oldTemplate
 from anvil import *
 import anvil.server
 import anvil.facebook.auth
@@ -10,9 +10,13 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class endringslogg(endringsloggTemplate):
+class endringslogg_old(endringslogg_oldTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Loggbok")
